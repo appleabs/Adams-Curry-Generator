a678b8348b671ab27155e4122997cb333922f90b
Lewis Blackburn <blackbul@aston.ac.uk>
2017-02-25 22:43:50 +0000

    Added implementation of getRandomRecipe() which returns a random curry from the Mongo database.

--- AdamsCurryGenerator/src/uk/curryGenerator/MongoConnector.java
+++ AdamsCurryGenerator/src/uk/curryGenerator/MongoConnector.java
@@ -3,14 +3,19 @@ package uk.curryGenerator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
+import com.mongodb.AggregationOutput;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 /**
  * class responsible for connecting to an instance of a mongo database
- * @author purple
+ * @author thirdratecyberpunk
  *
  */
 public class MongoConnector{
@@ -20,6 +25,7 @@ public class MongoConnector{
 	DB recipes;
 	// collection of curry recipes
 	DBCollection curries;
+
 	/**
 	 * constructor
 	 */
@@ -33,10 +39,35 @@ public class MongoConnector{
 
 
 	/**
-	 * retrieves a random recipe from the database
+	 * gets a random recipe from the database and returns it to the interface as a map
 	 */
-	public void getRandomRecipe(){
-		System.out.println("Hi!");
+	public Map getRandomRecipe(){
+		// TODO: implement aggregation method, but this requires MongoDB 3.6 apparently
+		// https://docs.mongodb.com/manual/aggregation/
+		// create pipeline $sample operation
+		// BasicDBObject sample = new BasicDBObject("$sample", new BasicDBObject("size", "1"));
+		// get a single recipe from the database
+		// AggregationOutput result = curries.aggregate(sample);
+		
+		// current implementation is far less clean but >hackathons >clean code
+		// perform query on database to pull item in this range
+		DBCursor cursor = curries.find();
+		// generate random number within range of length of database contents
+		Random random = new Random();
+		// this is a disgusting idea and if this somehow has a billion curries will be regrettable
+		// but yeah, no mongo 3.6
+		List<DBObject> docs = cursor.toArray();
+		int recipe = random.nextInt(docs.size());
+		DBObject recipeObject = docs.get(recipe);
+		Map recipeMap = recipeObject.toMap();
+		return recipeMap;
+	}
+
+	/*
+	 * gets recipes which contain the specified ingredients and returns a random one
+	 */
+	public void getIngredientRecipe(){
+
 	}
 
 	/**
@@ -53,6 +84,5 @@ public class MongoConnector{
 		newRecipe.put("steps", steps);
 		newRecipe.put("strength", strength);
 		curries.insert(newRecipe);
-		System.out.println("Should have written new recipe to database");
 	}
 }
