43e0b72b3b76670a3c4fdc2342320394dd2c3147
Lewis Blackburn <blackbul@aston.ac.uk>
2017-02-26 07:37:09 +0000

    Added ingredients title to generated checkbox

--- AdamsCurryGenerator/src/uk/curryGenerator/IngredientsCheckBoxGenerator.java
+++ AdamsCurryGenerator/src/uk/curryGenerator/IngredientsCheckBoxGenerator.java
@@ -7,6 +7,7 @@ import java.awt.GridBagConstraints;
 import java.util.ArrayList;
 
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.GridBagLayout;
 
@@ -21,15 +22,17 @@ public class IngredientsCheckBoxGenerator {
 		MongoConnector mc = new MongoConnector();
 		//dynamically create check boxes for ingredients
 		JPanel ingredientsPanel = new JPanel(new GridBagLayout());
-		int i = 0;
+		JLabel title = new JLabel("Ingredients");
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.gridx = 0;
+		constraints.gridy = 0;
+		ingredientsPanel.add(title);
+		int i = 0;
 		ArrayList<String> ingredients = mc.getIngredients();
 		while(i < ingredients.size()){
 			JCheckBox individualCheckBox = new JCheckBox(ingredients.get(i));
-			constraints.gridy = i;
+			constraints.gridy++;
 			ingredientsPanel.add(individualCheckBox, constraints);
-			//checkBox.add(individualCheckBox);
 			i++;
 		}
 		return ingredientsPanel;
